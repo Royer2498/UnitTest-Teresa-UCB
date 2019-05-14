aeb876dd02c311e66280668d9a8743d3339f2da9
3aVarana <3av.arana@gmail.com>
2019-05-14 18:36:34 -0400

    Primer test

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,11 +1,2 @@
-class Membership < ActiveRecord::Base
-  belongs_to :resume
-  before_create :validate_date
-  def validate_date
-    date<Time.now
-  end
-  def user
-    resume=self.resume
-    user=resume.user
-  end
+class Membership
 end
---
+++ spec/membership_spec.rb
@@ -0,0 +1,8 @@
+require './app/models/membership'
+require 'spec_helper'
+
+describe Membership do
+  it "The say_hello method should return 'Hello World'" do
+    expect("4").to eq "4" 
+  end
+end 
